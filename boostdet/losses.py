b761f7d9c4db95672be17746b3e4a55d3e006a36
yanghao <yanghao@qcraft.ai>
2023-05-30 21:52:00 +0800

    [BoostDet] update simple code of boostdet

--- boostdet/losses.py
+++ boostdet/losses.py
@@ -1,5 +1,11 @@
 import torch
 import torch.nn as nn
+from losses.gfocal_loss import DistributionFocalLoss, QualityFocalLoss
+from losses.iou_loss import GIoULoss, bbox_overlaps
+import torch.nn.functional as F
+
+import torch
+import torch.nn as nn
 import torch.nn.functional as F
 
 import functools
@@ -796,3 +802,224 @@ class CIoULoss(nn.Module):
             **kwargs,
         )
         return loss
+
+
+class Integral(nn.Module):
+    """A fixed layer for calculating integral result from distribution.
+    This layer calculates the target location by :math: `sum{P(y_i) * y_i}`,
+    P(y_i) denotes the softmax vector that represents the discrete distribution
+    y_i denotes the discrete set, usually {0, 1, 2, ..., reg_max}
+    Args:
+        reg_max (int): The maximal value of the discrete set. Default: 16. You
+            may want to reset it according to your new dataset or related
+            settings.
+    """
+
+    def __init__(self, reg_max=16):
+        super(Integral, self).__init__()
+        self.reg_max = reg_max
+        self.register_buffer(
+            "project", torch.linspace(0, self.reg_max, self.reg_max + 1)
+        )
+
+    def forward(self, x):
+        """Forward feature from the regression head to get integral result of
+        bounding box location.
+        Args:
+            x (Tensor): Features of the regression head, shape (N, 4*(n+1)),
+                n is self.reg_max.
+        Returns:
+            x (Tensor): Integral result of box locations, i.e., distance
+                offsets from the box center in four directions, shape (N, 4).
+        """
+        shape = x.size()
+        x = F.softmax(x.reshape(*shape[:-1], 4, self.reg_max + 1), dim=-1)
+        x = F.linear(x, self.project.type_as(x)).reshape(*shape[:-1], 4)
+        return x
+
+class GeneralizedFocalLoss(nn.Module):
+    def __init__(self, reg_max = 7, use_sigmoid=True, num_classes=4):
+        # 模型输出(多个list): [x16_logits], [x16_reg];
+        # 原始targets bbox: B, N, 5 动态shape
+        self.reg_max = reg_max
+        self.num_classes = num_classes
+        self.use_sigmoid = use_sigmoid
+        self.regression_scale = torch.nn.Linear(self.num_classes, self.num_classes)
+        self.distribution_project = Integral(self.reg_max)
+        self.loss_qfl = QualityFocalLoss(
+            use_sigmoid=self.use_sigmoid,
+            beta=2,
+            loss_weight=0.25,
+        )
+        self.loss_dfl = DistributionFocalLoss(
+            loss_weight=0.50
+        )
+        self.loss_bbox = GIoULoss(loss_weight=0.25)
+    
+    
+    def assign_targets(self,
+        label_infos, # bbox_targets: B, N, 5
+        stride = 16,
+        image_size = (256, 256),
+        ):
+        B = len(label_infos)
+        feat_size = (image_size[0] // stride, image_size[1] // stride)
+        cls_targets = []
+        bbox_targets = []
+        num_total_samples = 0
+        for i, label_info in enumerate(label_infos):
+            cls_target = torch.new_ones((feat_size[0], feat_size[1]), dtype=torch.long) * self.num_classes # H, W
+            bbox_target = torch.new_zeros((feat_size[0], feat_size[1], self.num_classes,  4), dtype=torch.float32) # H, W, num_cls, 4
+            for bbox_info in label_info:
+                x1, y1, x2, y2 = bbox_info['bbox']
+                category = bbox_info['category']
+                cx = (x1 + x2)/2.0
+                cy = (y1 + y2)/2.0
+                # 每个bbox，会被分配给最近的点 ----> 一个点可能会分配多个bbox
+                for offset_x in range(0, 2):
+                    for offset_y in range(0, 2):
+                        grid_cx = int(cx / stride) + offset_x
+                        grid_cy = int(cy / stride) + offset_y
+                        if grid_cx >= feat_size[0] or grid_cy >= feat_size[1]:
+                            continue
+                        if grid_cy < 0 or grid_cy < 0:
+                            continue
+                        cls_target[grid_cx, grid_cy] = category # 0 - num_cls - 1 for objects, num_cls for background
+                        num_total_samples += 1
+
+                        # 计算每个位置, 相对中心位置的偏移量 ---> 由于中心点不一定在bbox内，所以回归坐标是存在负值的情况的
+                        left_offset = grid_cx - x1 / stride  # 上下左右坐标偏移量，从当前点，向边界点移动；符合直觉
+                        right_offset = x2 / stride - grid_cx
+                        top_offset = grid_cy - y1/stride
+                        bottom_offset = y2/stride - grid_cy
+                        bbox_target[grid_cx, grid_cy, category, 0] = left_offset
+                        bbox_target[grid_cx, grid_cy, category, 1] = right_offset
+                        bbox_target[grid_cx, grid_cy, category, 2] = top_offset
+                        bbox_target[grid_cx, grid_cy, category, 3] = bottom_offset
+                        
+                        # 回归的目标是每个坐标线，相对中心点的偏移量
+                        # 但是，中间还涉及到target层面的scale
+                        # 如果target层面有可学习的target scale参数，能够更好地处理
+            cls_targets.append(cls_target)
+            bbox_targets.append(bbox_target)
+        return cls_targets, bbox_targets, num_total_samples
+
+    def loss_single(
+        self,
+        cls_score, # logits before sigmoid: N, num_classes, H, W
+        bbox_pred, # logits before sigmoid: N, 4 * num_classes *(reg_max + 1), H, W
+        cls_target, # N, H, W
+        bbox_targets, # N, H, W, num_classes, 4
+        stride,
+        num_total_samples,
+    ):
+
+        # cls_score = cls_score.reshape(-1, self.num_classes)  # N, num_classes, H, W
+        # bbox_pred = bbox_pred.reshape(-1, self.num_classes, 4 * (self.reg_max + 1)) # regrssion ---> 不同类别用的相同的回归，这里是不合理的
+        bbox_pred = bbox_pred.permute([0, 2, 3, 1]).reshape(-1, self.num_classes, 4 * (self.reg_max + 1)) # N, C, H, W ---> N, H, W, C
+        cls_score = cls_score.permute([0, 2, 3, 1]).reshape(-1, self.num_classes)
+
+        cls_target = cls_target.reshape(-1,) # N x H x W --> NHW, -1 每个位置的类别index
+        bbox_targets = bbox_targets.reshape(-1, self.num_classes, 4) # NHW, self.num_classes, 4
+        bbox_targets = self.regression_scale(bbox_targets) # NHW, self.num_classes, 4
+
+        # FG cat_id: [0, num_classes -1], BG cat_id: num_classes
+        bg_class_ind = self.num_classes # 每个位置会分配一个类别Index
+        pos_index = torch.nonzero(
+            (cls_target >= 0) & (cls_target < bg_class_ind), as_tuple=False
+        ).squeeze(1) # N x 1
+
+
+
+        score = bbox_pred.new_zeros(bbox_targets.shape)
+
+        if len(pos_index) > 0: # 有正样本
+            # 回归时，只回归正样本的bbox坐标
+            pos_bbox_targets = bbox_targets[pos_index] # (n, self.num_classes, 4) # 这里的regression target是相对于feature map的偏移量
+            pos_bbox_pred = bbox_pred[pos_index]  # (n, self.num_classes, 4 * (reg_max + 1))
+
+            # 按照类别映射，并按照类别进行scale ---> 根据target进行映射
+            # pos_bbox_targets = torch.gather(pos_bbox_targets, dim=1, index=cls_target)
+            # pos_bbox_pred = torch.gather(pos_bbox_pred, dim=1, index=cls_target)
+            pos_bbox_targets=torch.index_select(pos_bbox_targets,dim=1,index=cls_target)
+            pos_bbox_pred = torch.index_select(pos_bbox_pred, dim=1, index=cls_target)
+
+
+            # 获取score预测值，作为weight
+            weight_targets = cls_score.detach().sigmoid()
+            weight_targets = weight_targets.max(dim=1)[0][pos_index]
+
+            # dfl loss
+            loss_dfl = self.loss_dfl(
+                pos_bbox_pred,
+                pos_bbox_targets,
+                weight=weight_targets[:, None].expand(-1, 4).reshape(-1),
+                avg_factor=4.0,
+            )
+
+            # 计算IoU损失
+            loss_bbox = bbox_pred.sum() * 0
+            # pos_bbox_pred_corners = self.distribution_project(pos_bbox_pred)
+            # pos_decode_bbox_pred = distance2bbox(
+            #     pos_grid_cell_centers, pos_bbox_pred_corners
+            # )
+            # pos_decode_bbox_targets = pos_bbox_targets / stride
+            # score[pos_inds] = bbox_overlaps(
+            #     pos_decode_bbox_pred.detach(), pos_decode_bbox_targets, is_aligned=True
+            # )
+            # pred_corners = pos_bbox_pred.reshape(-1, self.reg_max + 1)
+            # target_corners = bbox2distance(
+            #     pos_grid_cell_centers, pos_decode_bbox_targets, self.reg_max
+            # ).reshape(-1)
+
+            # # IoU loss
+            # loss_bbox = self.loss_bbox(
+            #     pos_decode_bbox_pred,
+            #     pos_decode_bbox_targets,
+            #     weight=weight_targets,
+            #     avg_factor=1.0,
+            # )
+            
+            # Cls Loss
+            loss_qfl = bbox_pred.sum() * 0
+
+        else: # 没有正样本
+            loss_bbox = bbox_pred.sum() * 0
+            loss_dfl = bbox_pred.sum() * 0
+            loss_qfl = bbox_pred.sum() * 0
+            weight_targets = torch.tensor(0).to(cls_score.device)
+
+        # qfl loss
+        # loss_qfl = self.loss_qfl(
+        #     cls_score,
+        #     (cls_target, score),
+        #     weight=None,
+        #     avg_factor=num_total_samples,
+        # )
+
+        return loss_qfl, loss_bbox, loss_dfl, weight_targets.sum()
+
+    def forward(self, outputs, label_info):
+        cls_score, bbox_pred = outputs # 假设只有一个level的输出
+        stride = 16 # 这个level的stride
+        image_zize = (256, 256)
+
+        # 第一步，分配target
+        cls_target, bbox_targets, num_total_samples = self.assign_targets(label_info, stride, image_zize)
+        num_total_samples = max(num_total_samples, 1.0)
+
+        # 第二步，计算每部分的loss
+        loss_qfl, loss_bbox, loss_dfl, avg_factor = self.loss_single(
+            cls_score, # logits before sigmoid: N, num_classes, H, W
+            bbox_pred, # logits before sigmoid: N, 4 * num_classes *(reg_max + 1), H, W
+            cls_target, #
+            bbox_targets,
+            stride,
+            num_total_samples,
+        )
+
+        # 第三步，计算总loss
+        avg_factor = max(1.0, sum(avg_factor).item())
+        loss_bbox = loss_bbox / avg_factor  #IoU损失，与bbox个数有关
+        loss_dfl = loss_dfl / avg_factor # DFL损失，与bbox个数有关
+        return loss_qfl, loss_bbox, loss_dfl
--- boostdet/models.py
+++ boostdet/models.py
@@ -7,6 +7,7 @@ from sklearn.metrics import average_precision_score
 from torchvision.ops import box_iou
 import numpy as np
 import cv2
+from losses import GeneralizedFocalLoss
 
 class FCOS(nn.Module):
     def __init__(self, num_classes):
@@ -45,33 +46,17 @@ class FCOS(nn.Module):
         # 直接在输入部分加入embeddig? 也可以在输出部分加入embedding
         x_embedding = torch.arange(256, dtype=torch.float32).unsqueeze(0).unsqueeze(0).unsqueeze(0)/256.0 # 1, 1, 1, 256
         y_embedding = torch.arange(256, dtype=torch.float32).unsqueeze(0).unsqueeze(0).unsqueeze(-1)/256.0 # 1, 1, 256, 1
-        x_embedding = x_embedding.repeat(1, 1, 256, 1) # 1, 256, 256, 1
-        y_embedding = y_embedding.repeat(1, 1, 1, 256) # 1, 1, 256, 256
+        x_embedding = x_embedding.expand(-1, -1, 256, -1) # 1, 256, 256, 1
+        y_embedding = y_embedding.expand(-1, -1, -1, 256) # 1, 1, 256, 256
         xy_embedding = torch.cat([x_embedding, y_embedding], dim=1) # 1, 2, 256, 256
-        
+        self.position_embedding = nn.Parameter(xy_embedding, requires_grad=True)
 
-        self.position_embedding = nn.Parameter(xy_embedding, requires_grad=False)
         # Define the classification, regression, and centerness heads
         in_channels = 256
-        # 分类的head, 用于判断当前位置，是否包含目标； 后面会添加sigmoid，然后预测IoU的值 ----> 可以得到概率分布图
-        # 目标anchor的大小选择: 原图每个位置可以被覆盖两次； (每个位置的感受野，实际上是大于anchor的大小的)
-        # sigmoid不利于部署，因此在计算图缩放时，使用sigmoid前的logits来实现；
-
-        # self.rpn_head = nn.Conv2d(in_channels, self.num_classes, kernel_size=3, stride=1, padding=1)
-        
-        # 回归的head, 用于预测目标的边界框位置
-        # 用的是softmax的输出，乘以一个stride数，得到最终的边界位置
-        # 方案一: nanodet的方案。在缩放后的feature map上，从中心向四周扩散，用softmax计算最终输出； --->softmax无法去除，不利于部署
-        # 方案二: 回归x,y,w,h? 不符合直觉，因为我们不会去判断中心点在哪里，而是判断边界框的位置；
-        # 方案三: 分别回归上下左右边界的偏移量，符合预期； 先直接用smooth-l1 loss来计算；
-        #        直接回归x1,x2,y1,y2的normalized的值，效果不好；
-        
-        # 5个输出，分别预测每个位置，上下左右位置的相对值(0-1之间)，最后一个用于计算IoU loss；
-        self.reg_head = nn.Conv2d(in_channels, self.num_classes * 4, kernel_size=3, stride=1, padding=1)
-        self.cls_head = nn.Conv2d(in_channels, self.num_classes * 1, kernel_size=3, stride=1, padding=1)
-
-        # TODO: 使用setcriterion来计算loss，从而去掉NMS过程； ---> 先考虑密集检测
-        # TODO: 使用multi-level的输出； 先用分类score map逐级refine，最后在最大的feature map上进行回归；
+        reg_max = 7
+        self.cls_head = nn.Conv2d(in_channels, self.num_classes, kernel_size=3, stride=1, padding=1)
+        self.reg_head = nn.Conv2d(in_channels, self.num_classes * 4 * (reg_max + 1), kernel_size=3, stride=1, padding=1)
+        self.gfl_loss = GeneralizedFocalLoss(reg_max=reg_max)
 
     def forward(self, x):
         # Forward pass through the backbone network
@@ -79,54 +64,17 @@ class FCOS(nn.Module):
         # import numpy as np
         # print(np.shape(x), np.shape(self.position_embedding)) # torch.Size([8, 3, 256, 256]) torch.Size([1, 2, 256, 256])
         B = x.size(0)
-        position_embedding = self.position_embedding.repeat((B, 1, 1, 1))
+        position_embedding = self.position_embedding.expand((B, -1, -1, -1))
         x = torch.cat([x, position_embedding], dim=1) 
-        
         x4 = self.backbone_stride4(x)
-        
         x16 = self.backbone_stride16(x4)
-        
-        # import numpy as np
-        # print("output feat shape", np.shape(x4), np.shape(x16))
-
-        # rpn_logits = self.rpn_head(x16) # 16x16x4 ----> 暂时用不
-        
-        # scaled_x4 = self.get_scale_codinates(score_logits, x4) # 对原图进行缩放, 缩放到x16的大小
-        
-        # # 然后用score_logits对更大的feature map进行缩放； ----> 这里直接使用128x128的feature map来进行缩放；
-        # x4_outputs = self.reg_head(scaled_x4) # TODO: 用score logits对x4进行缩放；这里不能和x16进行concat，二者语义是完全不同的，不存在对应关系；
+    
         x16_reg_logits = self.reg_head(x16)
         x16_cls_logits = self.cls_head(x16)
-        return rpn_logits, x16_reg_logits, x16_cls_logits # , x4_outputs
+        return x16_reg_logits, x16_cls_logits
 
     def loss(self, rpn_logits, x16_reg_logits, x16_cls_logits, targets):
-        
-        rpn_targets, reg_targets, reg_mask, bboxes = targets
+        loss_qfl, loss_bbox, loss_dfl  = self.gfl_loss(rpn_logits, x16_reg_logits, x16_cls_logits, targets)
+        loss = loss_qfl + loss_bbox + loss_dfl
+        return loss
 
-        # 分类IoU的loss
-        eps = 1e-5
-        preds = torch.clamp(torch.sigmoid(rpn_logits), eps, 1 - eps) # sigmoide获取概率
-        alpha = 0.5
-        gamma = 2
-        loss_rpn = - alpha * (1 - preds) ** gamma * rpn_targets * torch.log(preds) - (1 - alpha) * preds ** gamma * (1 - rpn_targets) * torch.log(1 - preds)
-        loss_rpn = torch.sum(loss_rpn)
-        print("rpn:")
-        print(preds)
-        print(rpn_targets)
-        exit(0)
-        # reg的loss
-        preds = torch.clamp(torch.sigmoid(x16_reg_logits), eps, 1 - eps) # B, num_cls * 4, 16, 16
-        alpha = 0.5
-        gamma = 2
-        loss_reg = - alpha * (1 - preds) ** gamma * reg_targets * torch.log(preds) - (1 - alpha) * preds ** gamma * (1 - reg_targets) * torch.log(1 - preds)
-        loss_reg = torch.sum(loss_reg * reg_mask)
-
-        # iou预测loss
-        # bboxes = preds * 256.0 # B, num_cls, 16, 16
-        # preds = torch.sigmoid(x16_cls_logits) # B, num_cls * 4, 16, 16
-        loss_dict = dict(
-            loss_rpn = loss_rpn,
-            loss_reg = loss_reg,
-            loss = loss_reg + loss_rpn
-        )
-        return loss_dict
